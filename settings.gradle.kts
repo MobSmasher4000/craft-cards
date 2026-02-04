f50ad9ad22caca7e4ae30de90d536c173f39389e
Zombiebear99 <dsehrawat521+mobsmasher4000+github@gmail.com>
2026-02-04 20:52:00 +0530

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+// By default, this is how your built jar is called
+rootProject.name = "Craft Cards NeoForge"
+
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.minecraftforge.net/")
+        gradlePluginPortal()
+    }
+}
